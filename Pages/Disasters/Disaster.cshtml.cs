5a291d95407c8fb6b1439aad8728e5e0548be6c1
Jeanne Margaret L. Guzon <guzonjeanne@gmail.com>
2023-09-20 18:11:57 +0800

    Added Disaster Page

--- Pages/Disasters/Disaster.cshtml.cs
+++ Pages/Disasters/Disaster.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AgapayAidSystem.Pages.Disaster
 {
-    public class _AddDisasterModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
