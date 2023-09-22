a839861fe6f441587db3d0dec6633791744bf562
Jeanne Margaret L. Guzon <guzonjeanne@gmail.com>
2023-09-22 14:41:52 +0800

    Create a Program Folder - with get set function.

---
+++ Pages/Disaster/Program/Class.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgapayAidSystem.Pages.Disaster.Program
+{
+	public class Class
+	{
+		[Key] // This annotation specifies that DisasterID is the primary key
+		public string DisasterID { get; set; }
+		[Required]
+
+		public string DisasterName { get; set; }
+		public DisasterType disasterType { get; set; }
+		public string Description { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+	}
+
+	public enum DisasterType
+	{
+		Typhoon,
+		Flood,
+		Landslide,
+		Earthquake,
+		VolcanicEruption,
+		Tsunami,
+		Fire,
+		IndustrialAccident,
+		Epidemic
+	}
+}
