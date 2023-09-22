066504ba4c88c6bca7e8c8f415851cfffd857b14
Gwen <agathagwen06@gmail.com>
2023-09-22 18:45:44 +0800

    edit1

---
+++ Pages/UserManagement/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.UserManagement
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
