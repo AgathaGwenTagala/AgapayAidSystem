227163b3af3df7c5b361624e7bf3a7c43df4c79a
Gwen <agathagwen06@gmail.com>
2023-09-23 20:09:51 +0800

    Updated CreateECStaff

--- Pages/UserManagement/CreateECStaff.cshtml.cs
+++ Pages/UserManagement/CreateECStaff.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Net;
 
 namespace AgapayAidSystem.Pages.UserManagement
 {
     public class CreateECStaffModel : PageModel
     {
+        public UserInfo userInfo { get; set; } = new UserInfo();
+        public string userID { get; set; } = "";
+        public string firstName { get; set; } = "";
+        public string middleName { get; set; } = "";
+        public string lastName { get; set; } = "";
+        public string extName { get; set; } = "";
+        public string sex { get; set; } = "";
+        public string birthdate { get; set; } = "";
+        public string mobileNum { get; set; } = "";
+        public string emailAddress { get; set; } = "";
+        public string errorMessage = "";
+        public string successMessage = "";
+
+
         public void OnGet()
         {
+
+        }
+
+        public void OnPost() 
+        {
+            
         }
     }
 }
