738433128081be9e25a0f0423f393b2018cf5f19
Gwen <agathagwen06@gmail.com>
2023-09-23 17:40:37 +0800

    Can pass userID from Create to CreateAdmin

--- Pages/UserManagement/Create.cshtml.cs
+++ Pages/UserManagement/Create.cshtml.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
-
+using System.Net;
 
 namespace AgapayAidSystem.Pages.UserManagement
 {
 	public class CreateModel : PageModel
     {
-		public UserInfo userInfo = new UserInfo();
+		public UserInfo userInfo { get; set; } = new UserInfo();
+		public string userID { get; set; } = "";
 		public String errorMessage = "";
 		public String successMessage = "";
 
@@ -23,7 +24,7 @@ namespace AgapayAidSystem.Pages.UserManagement
 				return;
 			}
 
-			// Get the selected userType directly from the form
+			// Retrieve the selected userType from the form
 			userInfo.userType = Request.Form["userType"];
 
 			// Validate userType
@@ -39,41 +40,65 @@ namespace AgapayAidSystem.Pages.UserManagement
 				return;
 			}
 
-			// Save the new user into the database
-			try
+            // Initialize a variable to store the last inserted userID
+            string lastInsertedUserID = "";
+
+            // Save the new user into the database
+            try
 			{
 				string connectionString = "server=localhost;user=root;database=agapayaid;port=3306;password=12345;";
 				using (MySqlConnection connection = new MySqlConnection(connectionString))
 				{
 					connection.Open();
-					String sql = "INSERT INTO user (userType) VALUES (@userType);";
 
+                    // Insert the userType into the 'user' table
+                    String sql = "INSERT INTO user (userType) VALUES (@userType);";
 					using (MySqlCommand command = new MySqlCommand(sql, connection))
 					{
 						command.Parameters.AddWithValue("@userType", userInfo.userType);
 						command.ExecuteNonQuery();
 					}
-				}
-			}
+
+                    // Fetch the last inserted userID based on userType
+                    string selectSql = "SELECT userID FROM user WHERE userType = @userType ORDER BY createdAt DESC LIMIT 1;";
+                    using (MySqlCommand selectCommand = new MySqlCommand(selectSql, connection))
+                    {
+                        selectCommand.Parameters.AddWithValue("@userType", userInfo.userType);
+                        lastInsertedUserID = selectCommand.ExecuteScalar()?.ToString();
+                    }
+                }
+
+                
+            }
 			catch (Exception ex)
 			{
 				errorMessage = ex.Message;
 				return;
 			}
 
+
             // Redirect to respective pages based on userType
-            if (userInfo.userType == "Admin")
+            // Check if lastInsertedUserID has a valid value
+            if (!string.IsNullOrEmpty(lastInsertedUserID))
             {
-                Response.Redirect("/UserManagement/CreateAdmin");
+                if (userInfo.userType == "Admin")
+                {
+                    Response.Redirect($"/UserManagement/CreateAdmin?userID={WebUtility.UrlEncode(lastInsertedUserID)}");
+                }
+                else if (userInfo.userType == "LGU Staff")
+                {
+                    Response.Redirect($"/UserManagement/CreateLGUStaff?userID={WebUtility.UrlEncode(lastInsertedUserID)}");
+                }
+                else if (userInfo.userType == "EC Staff")
+                {
+                    Response.Redirect($"/UserManagement/CreateECStaff?userID={WebUtility.UrlEncode(lastInsertedUserID)}");
+                }
             }
-            else if (userInfo.userType == "LGU Staff")
+            else
             {
-                Response.Redirect("/UserManagement/CreateLGUStaff");
-            }
-            else if (userInfo.userType == "EC Staff")
-            {
-                Response.Redirect("/UserManagement/CreateECStaff");
-            }
+                // Handle the case where lastInsertedUserID is missing or invalid
+                errorMessage = "UserID is missing or invalid.";
+            }            
         }
-	}
+    }
 }
--- Pages/UserManagement/CreateAdmin.cshtml.cs
+++ Pages/UserManagement/CreateAdmin.cshtml.cs
@@ -1,12 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Net;
 
 namespace AgapayAidSystem.Pages.UserManagement
 {
     public class CreateAdminModel : PageModel
     {
-        public void OnGet()
-        {
+        public UserInfo userInfo { get; set; } = new UserInfo();
+        public string adminName { get; set; } = "";
+		public string userID { get; set; } = "";
+		public string errorMessage = "";
+		public string successMessage = "";
+
+		public void OnGet(string lastInsertedUserID)
+		{
+            // Retrieve and decode the userID from the query string
+            string encodedUserID = HttpContext.Request.Query["userID"];
+            userID = WebUtility.UrlDecode(encodedUserID);
+
+            // Ensure that userID is not empty
+            if (string.IsNullOrEmpty(lastInsertedUserID))
+			{
+				errorMessage = "UserID is missing.";
+				return;
+			}
+
+			// Retrieve the userID passed from the Create page
+			// userInfo.userID = lastInsertedUserID;
+
+            
         }
-    }
+
+		public void OnPost()
+		{
+            if (!ModelState.IsValid)
+			{
+				errorMessage = "Please correct the errors below.";
+				return;
+			}
+
+			// Retrieve the userID from the form
+			string userID = Request.Form["userID"];
+            string adminName = Request.Form["adminName"];
+
+            // Check if userID is null or empty
+            if (string.IsNullOrEmpty(userID))
+			{
+				errorMessage = "UserID is missing.";
+				return;
+			}
+
+            try
+			{
+				string connectionString = "server=localhost;user=root;database=agapayaid;port=3306;password=12345;";
+				using (MySqlConnection connection = new MySqlConnection(connectionString))
+				{
+					connection.Open();
+
+					// Insert data into the 'admin' table
+					string sql = "INSERT INTO admin (userID, adminName) VALUES (@userID, @adminName);";
+					using (MySqlCommand command = new MySqlCommand(sql, connection))
+					{
+						command.Parameters.AddWithValue("@userID", userID);
+						command.Parameters.AddWithValue("@adminName", adminName);
+						command.ExecuteNonQuery();
+					}
+				}
+
+				successMessage = "Admin user created successfully!";
+			}
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+				return;
+			}
+
+			Response.Redirect("/UserManagement/Index");
+		}
+	}
 }
