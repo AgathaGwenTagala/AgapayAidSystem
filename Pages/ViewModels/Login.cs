d1c8ac2a29de480bbda50cc2f90a3fa058db0bc9
Ian Vincent Bartolome <ianvincentbartolome16@gmail.com>
2023-09-24 16:08:21 +0800

    delete

---
+++ Pages/ViewModels/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AgapayAidSystem.Pages.ViewModels
+{
+	public class Login
+	{
+		[Required]
+		public string UserName { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
