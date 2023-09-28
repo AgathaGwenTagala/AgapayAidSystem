5c361394e46708911e5c33f0b621bc88adc35708
Gwen <agathagwen06@gmail.com>
2023-09-28 21:24:35 +0800

    Added Edit pages for each userType

---
+++ Pages/UserManagement/EditAdmin.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace AgapayAidSystem.Pages.UserManagement
+{
+    public class EditAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+			
+        }
+
+        public void OnPost()
+        {
+			
+		}
+    }
+}
---
+++ Pages/UserManagement/EditLGUStaff.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.UserManagement
+{
+    public class EditLGUStaffModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
