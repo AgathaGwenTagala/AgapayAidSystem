87fc9733c6be8dd6a451a01e6ac16e74abffae5a
Gwen <agathagwen06@gmail.com>
2023-09-30 21:39:27 +0800

    Deleted _Allocate and _Edit

---
+++ Pages/Disaster/AllocationEvacuationCenter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.Disaster
+{
+    public class AllocationEvacuationCenterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
