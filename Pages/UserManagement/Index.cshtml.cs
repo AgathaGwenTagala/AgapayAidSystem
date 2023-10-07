c68f8742fd6b877c019ce7119772e4b9668037d8
Gwen <agathagwen06@gmail.com>
2023-10-07 20:22:04 +0800

    Added Search for UserMgmt

--- Pages/UserManagement/Index.cshtml.cs
+++ Pages/UserManagement/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using AgapayAidSystem.Pages.EvacuationCenter;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
@@ -36,7 +37,7 @@ namespace AgapayAidSystem.Pages.UserManagement
                                     userInfo.userPhoto = (byte[])reader.GetValue(4);
                                 }
 
-                                userInfo.createdAt = reader.GetDateTime(5).ToString();
+                                userInfo.createdAt = reader.GetDateTime(5).ToString("yyyy-MM-dd hh:mm:ss tt");
                                 listUsers.Add(userInfo);
                             }
                         }
@@ -49,7 +50,55 @@ namespace AgapayAidSystem.Pages.UserManagement
                 Console.WriteLine("Exception: " + ex.ToString());
             }
         }
-    }
+
+		public JsonResult OnGetSearch(string query)
+		{
+			try
+			{
+				string connectionString = "server=localhost;user=root;database=agapayaid;port=3306;password=12345;";
+				using (MySqlConnection connection = new MySqlConnection(connectionString))
+				{
+					connection.Open();
+					string sql = "SELECT * FROM user WHERE username LIKE @query OR userType LIKE @query OR createdAt LIKE @query";
+
+					using (MySqlCommand command = new MySqlCommand(sql, connection))
+					{
+						// Add query parameter with wildcard
+						command.Parameters.AddWithValue("@query", $"%{query}%");
+
+						using (MySqlDataReader reader = command.ExecuteReader())
+						{
+							List<UserInfo> searchResults = new List<UserInfo>();
+							while (reader.Read())
+							{
+								UserInfo userInfo = new UserInfo();
+								userInfo.userID = "" + reader.GetString(0);
+								userInfo.username = "" + reader.GetString(1);
+								userInfo.password = "" + reader.GetString(2);
+								userInfo.userType = "" + reader.GetString(3);
+
+								// Convert the userPhoto BLOB data to a byte[]
+								if (!reader.IsDBNull(4))
+								{
+									userInfo.userPhoto = (byte[])reader.GetValue(4);
+								}
+
+                                userInfo.createdAt = reader.GetDateTime(5).ToString("yyyy-MM-dd hh:mm:ss tt");
+
+                                searchResults.Add(userInfo);
+							}
+							return new JsonResult(searchResults);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception: " + ex.ToString());
+				return new JsonResult(new List<UserInfo>());
+			}
+		}
+	}
 
     public class UserInfo
     {
