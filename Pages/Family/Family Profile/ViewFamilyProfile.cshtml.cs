a546da9589aba7dc6381586c45df76b355a5cfc3
Ian Vincent Bartolome <ianvincentbartolome16@gmail.com>
2023-10-16 22:02:35 +0800

    View Family Profile

---
+++ Pages/Family/Family Profile/ViewFamilyProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.Family.Family_Profile
+{
+    public class ViewFamilyProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
