6093b2ad9835288eb455fd53836efd7250d1c0cd
Gwen <agathagwen06@gmail.com>
2023-10-18 21:37:09 +0800

    Added Family/Profile/Index page

---
+++ Pages/Family/Profile/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.Family.Profile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
