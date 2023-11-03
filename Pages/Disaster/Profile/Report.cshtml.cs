2ae61c19de145662f63dac24ce62bac1a96be644
Jeanne Margaret L. Guzon <guzonjeanne@gmail.com>
2023-11-03 21:08:58 +0800

    Added a post-disaster report page.

---
+++ Pages/Disaster/Profile/Report.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.Disaster.Profile
+{
+    public class ReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
