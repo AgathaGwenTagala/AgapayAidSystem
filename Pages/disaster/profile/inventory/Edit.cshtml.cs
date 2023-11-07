3f781c8448360c1116923102cce9fc2ab16c0431
Gwen <agathagwen06@gmail.com>
2023-11-07 17:34:20 +0800

    Inventory index done

---
+++ Pages/disaster/profile/inventory/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.disaster.profile.inventory
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
