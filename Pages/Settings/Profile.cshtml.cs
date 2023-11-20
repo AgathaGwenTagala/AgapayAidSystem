f0895c0d48a346a819ce37ff325520bc80590d4b
Ian Vincent Bartolome <ianvincentbartolome16@gmail.com>
2023-11-20 23:38:44 +0800

    settings

---
+++ Pages/Settings/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.Settings
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
