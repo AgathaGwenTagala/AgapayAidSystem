4a562363f9072fcf2c492d5836aa7a43e58c4e34
Gwen <agathagwen06@gmail.com>
2023-11-23 23:35:12 +0800

    Removed contact us

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -14,53 +14,8 @@ namespace AgapayAidSystem.Pages
             _logger = logger;
         }
 
-        [BindProperty]
-        public string SenderEmail { get; set; }
-
-        [BindProperty]
-        public string Name { get; set; }
-
-        [BindProperty]
-        public string Message { get; set; }
-
         public void OnGet()
         {
-        }
-
-        public void OnPost()
-        {
-            // Your email configuration
-            var smtpClient = new SmtpClient("smtp.gmail.com")
-            {
-                Port = 587,
-                Credentials = new NetworkCredential("agapayaidproject@gmail.com", "mdaitttqkqolujdz"),
-                EnableSsl = true,
-            };
-
-            // Email content
-            var mailMessage = new MailMessage
-            {
-                From = new MailAddress(SenderEmail), // Use the sender's email from the form
-                Subject = "Message from AgapayAid Contact Form",
-                Body = $"Name: {Name}\n\nMessage: {Message}",
-                IsBodyHtml = false,
-            };
-
-            // Recipient email address
-            mailMessage.To.Add("agapayaidproject@gmail.com");
-
-            try
-            {
-                // Send the email
-                smtpClient.Send(mailMessage);
-                _logger.LogInformation("Email sent successfully.");
-                Response.Redirect("/about?success=true");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error sending email.");
-                Response.Redirect("/about?error=" + ex.Message);
-            }
         }
     }
 }
