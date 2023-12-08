f01b9b7cf1f0c615bfe3c137993743e7ec691836
Gwen <agathagwen06@gmail.com>
2023-12-08 22:59:33 +0800

    added view assignment history

---
+++ Pages/ecstaff/AssignmentHistory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgapayAidSystem.Pages.ecstaff
+{
+    public class AssignmentHistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
