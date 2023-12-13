871bafe770189979b5229344d7391442b58879bc
Jeanne Margaret L. Guzon <guzonjeanne@gmail.com>
2023-12-14 00:53:50 +0800

    initial edit for the about us.

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -8,6 +8,8 @@ namespace AgapayAidSystem.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        public string UserId { get; set; }
+        public string UserType { get; set; }
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
@@ -16,6 +18,9 @@ namespace AgapayAidSystem.Pages
 
         public void OnGet()
         {
+            // Check if UserId is set in the session
+            UserId = HttpContext.Session.GetString("UserId");
+            UserType = HttpContext.Session.GetString("UserType");
         }
     }
 }
