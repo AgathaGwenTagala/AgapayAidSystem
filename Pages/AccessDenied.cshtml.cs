0f99d7bcc070859b1c7831878f4a781b8144ced9
Gwen <agathagwen06@gmail.com>
2023-12-14 01:30:14 +0800

    added session in access denied page

--- Pages/AccessDenied.cshtml.cs
+++ Pages/AccessDenied.cshtml.cs
@@ -5,8 +5,14 @@ namespace AgapayAidSystem.Pages
 {
     public class AccessDeniedModel : PageModel
     {
+        public string UserId { get; set; }
+        public string UserType { get; set; }
+
         public void OnGet()
         {
+            // Check if UserId is set in the session
+            UserId = HttpContext.Session.GetString("UserId");
+            UserType = HttpContext.Session.GetString("UserType");
         }
     }
 }
