1f281f40ab03d7aab398477ccb0f541780a6f191
Gwen <agathagwen06@gmail.com>
2023-12-19 17:43:16 +0800

    Minor change

--- Pages/Login/Index.cshtml.cs
+++ Pages/Login/Index.cshtml.cs
@@ -84,7 +84,7 @@ namespace AgapayAidSystem.Pages.Login
 							}
 						}
                         
-                        Console.WriteLine($"Retrieved username [{username}] with type [{userInfo.userType}]");
+                        // Console.WriteLine($"Retrieved username [{username}] with type [{userInfo.userType}]");
                     }
                 }
             }
